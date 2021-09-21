9db61faebdc9bdac60d57bc93e4e290aff851ba5
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-09-21 08:39:57 -0500

    Funcinalidad barra de navegacion y fragmentos

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/ContenedorFragmentos.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/ContenedorFragmentos.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContenedorFragmentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,37 @@ class ContenedorFragmentos : AppCompatActivity() {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<FragmentoListaCategorias>(R.id.fl_contenedor)
+            }
+        }
+
+        val navBar = findViewById<BottomNavigationView>(R.id.bnv_navbar)
+        navBar.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.mi_ir_notas ->{
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        replace<FragmentoListaCategorias>(R.id.fl_contenedor)
+                        addToBackStack(null)
+                    }
+                    true
+                }
+                R.id.mi_ir_temporizador -> {
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        replace<TemporizadorFragment>(R.id.fl_contenedor)
+                        addToBackStack(null)
+                    }
+                    true
+                }
+                R.id.mi_ir_todo -> {
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        replace<ToDoFragment>(R.id.fl_contenedor)
+                        addToBackStack(null)
+                    }
+                    true
+                }
+                else -> false
             }
         }
     }
