0206d7f3fd809f14f21b11afd8a6cbc456c42fb5
Santiago <santiago.villegas@epn.edu.ec>
2021-09-20 23:15:53 -0500

    Colores y dependencias para Facebook

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/MainActivity.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.proyecto2b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val LoginFacebook = findViewById<ImageView>(R.id.iv_loginFacebook)
+        val LoginGoogle = findViewById<ImageView>(R.id.iv_loginGoogle)
     }
 }
