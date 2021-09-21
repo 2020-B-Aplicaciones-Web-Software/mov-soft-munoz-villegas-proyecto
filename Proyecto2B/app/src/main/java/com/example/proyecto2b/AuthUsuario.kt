e40928e1d64a93ae256c9479f45a0cfccf251edf
Santiago <santiago.villegas@epn.edu.ec>
2021-09-21 10:41:25 -0500

    Adicion inicio de sesion

---
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/AuthUsuario.kt
@@ -0,0 +1,10 @@
+package com.example.proyecto2b
+
+class AuthUsuario {
+    companion object{
+        var usuario:UsuarioFirebase?
+        init{
+            this.usuario=null;
+        }
+    }
+}
