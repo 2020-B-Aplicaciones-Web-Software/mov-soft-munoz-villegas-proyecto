aa1b6ebc1bc007ff47075590c663d1fc1c7c1a30
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-09-21 09:56:54 -0500

    Creacion child fragment DatosTemporizador
    Se creo el nuevo fragmento y se lo inscrustó como fragmento inicial del fragmento temporizador padre

---
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/DatosTemporizador.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto2b
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.commit
+
+class DatosTemporizador : Fragment(R.layout.fragment_datos_temporizador) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_datos_temporizador, container, false)
+    }
+
+
+}
--- Proyecto2B/app/src/main/java/com/example/proyecto2b/TemporizadorFragment.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/TemporizadorFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class TemporizadorFragment : Fragment(R.layout.fragment_temporizador) {
     override fun onCreateView(
@@ -12,6 +14,12 @@ class TemporizadorFragment : Fragment(R.layout.fragment_temporizador) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        if(savedInstanceState == null){
+            childFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<DatosTemporizador>(R.id.fl_tempContainer)
+            }
+        }
         val viewRoot = inflater.inflate(R.layout.fragment_temporizador,container,false)
         return viewRoot
     }
