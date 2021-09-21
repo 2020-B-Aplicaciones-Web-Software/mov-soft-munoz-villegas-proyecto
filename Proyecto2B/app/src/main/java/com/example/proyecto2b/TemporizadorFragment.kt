7c3c5e76afc930512501d9033cf3dfb7be2d81bd
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-09-21 08:09:54 -0500

    Creacion Fragmento temporizador
    creacion de la clase del fragmento temporizador y su diseño xml

---
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/TemporizadorFragment.kt
@@ -0,0 +1,18 @@
+package com.example.proyecto2b
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TemporizadorFragment : Fragment(R.layout.fragment_temporizador) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val viewRoot = inflater.inflate(R.layout.fragment_temporizador,container,false)
+        return viewRoot
+    }
+}
