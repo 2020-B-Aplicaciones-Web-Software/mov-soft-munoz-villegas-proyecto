485752bc50729d253cdbbd7ac6a1ad06659b408b
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-09-21 00:10:58 -0500

    Creacion fragmento lista categorias

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/ContenedorFragmentos.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/ContenedorFragmentos.kt
@@ -2,10 +2,18 @@ package com.example.proyecto2b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class ContenedorFragmentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contenedor_fragmentos)
+        if( savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<FragmentoListaCategorias>(R.id.fl_contenedor)
+            }
+        }
     }
 }
---
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/FragmentoListaCategorias.kt
@@ -0,0 +1,20 @@
+package com.example.proyecto2b
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+
+class FragmentoListaCategorias : Fragment(R.layout.fragment_fragmento_lista_categorias) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val viewRoot = inflater.inflate(R.layout.fragment_fragmento_lista_categorias,container,false)
+        return viewRoot
+    }
+}
