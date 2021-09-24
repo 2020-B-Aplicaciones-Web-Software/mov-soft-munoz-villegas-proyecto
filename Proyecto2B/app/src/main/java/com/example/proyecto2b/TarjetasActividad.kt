b71b2c537029f64f5f00c783e963aa635978472c
Santiago <santiago.villegas@epn.edu.ec>
2021-09-24 18:56:38 -0500

    terminado creación de tarjetas

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/TarjetasActividad.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/TarjetasActividad.kt
@@ -6,40 +6,69 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto2b.Dto.FirestoreCategoriaDto
 import com.example.proyecto2b.Dto.FirestoreTarjetaDto
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class TarjetasActividad : AppCompatActivity() {
     var listaTarjetas= arrayListOf<FirestoreTarjetaDto>()
+    lateinit var categoria:FirestoreCategoriaDto
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tarjetas_actividad)
-        cargarnterfaz()
+        categoria= intent.getParcelableExtra<FirestoreCategoriaDto>("categoria")!!
+        cargarTarjetas()
+
 
 
     }
-    fun cargarnterfaz(){
-        val categoria=intent.getParcelableExtra<FirestoreCategoriaDto>("categoria")
+    fun cargarTarjetas(){
+        listaTarjetas.clear()
+
+        val db= Firebase.firestore
+
+        val referencia = db.collection("usuario").document(AuthUsuario.usuario!!.email)
+            .collection("categoria").document(categoria.uid!!)
+            .collection("tajeta")
+            .orderBy("titulo", Query.Direction.DESCENDING)
+
+        referencia
+            .get()
+            .addOnSuccessListener { tarjetas ->
+                for (tajeta in tarjetas) {
+
+                    if (tajeta != null) {
+                        listaTarjetas.add(
+                            FirestoreTarjetaDto(tajeta.id,
+                                tajeta["titulo"].toString(),
+                                tajeta["descripcion"].toString()
+
+                            )
+                        )
+
+                    }
+
+                }
+                cargarInterfaz()
+            }
+        cargarInterfaz()
+
+    }
+    fun cargarInterfaz(){
+
         findViewById<TextView>(R.id.tv_tituloCategoria).setText(categoria?.nombre)
         Log.i("s","${categoria!!.nombre } ${categoria!!.uid }")
         val recyclerViewTarjetas=findViewById<RecyclerView>(
             R.id.rv_tarjetas
         )
-
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
-        listaTarjetas.add(FirestoreTarjetaDto("s","sssss","aaaaa"))
+        
 
         iniciarRecyclerView(listaTarjetas,this,recyclerViewTarjetas)
         val botonRegresar=findViewById<ImageView>(R.id.btn_regresarTarjetas)
@@ -61,12 +90,44 @@ class TarjetasActividad : AppCompatActivity() {
             .setView(dialogoView)
         val categoriaDialogo=builder.show()
         val botonAceptar=dialogoView.findViewById<Button>(R.id.btn_crearTajeta_dialogo)
-        val textoDescripcion=dialogoView.findViewById<EditText>(R.id.et_titulo_tarjeta_dialogo2)
-        val textoTitulo=dialogoView.findViewById<EditText>(R.id.et_titulo_tarjeta_dialogo)
+        val tituloTarjeta=dialogoView.findViewById<TextInputEditText>(R.id.til_nombre_tarjeta_dialogo)
+        val descripcionTarjeta=dialogoView.findViewById<TextInputEditText>(R.id.til_descripcion_tarjeta_dialogo)
+
+
 
         botonAceptar.setOnClickListener {
-            categoriaDialogo.dismiss()
+            if(tituloTarjeta.text.toString().length<=130
+                && tituloTarjeta.text.toString().length>=5
+                && descripcionTarjeta.text.toString().length<=130
+                && descripcionTarjeta.text.toString().length>=5){
+                    registrarTarjeta(tituloTarjeta.text.toString()
+                        ,descripcionTarjeta.text.toString()
+                        ,categoriaDialogo)
+
+            }
+
         }
+    }
+
+    private fun registrarTarjeta(titulo: String, descripcion: String, categoriaDialogo: AlertDialog?) {
+        val nuevaTarjeta = hashMapOf<String, Any>(
+            "titulo" to titulo,
+            "descripcion" to descripcion
+
+        )
+        val db = Firebase.firestore
+        val referencia = db.collection("usuario").document(AuthUsuario.usuario!!.email)
+            .collection("categoria").document(categoria.uid!!)
+            .collection("tajeta")
+
+        referencia
+            .add(nuevaTarjeta)
+            .addOnSuccessListener {
+                categoriaDialogo!!.dismiss()
+                cargarTarjetas()
+
+            }.addOnFailureListener {}
+
     }
 
     fun abrirActividad(
